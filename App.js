f41d8150ba568ec3a01c943f4d412ea8561f2e18
AlbertJin1 <awsomegamingracerhd3@gmail.com>
2024-02-19 14:59:33 +0800

    SCRATCH

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//SCREENS
+import Home from './Screens/Home'
+import Konnichiwa from './JapaneseScreen/Konnichiwa'
+import Hola from './SpanishScreen/Hola'
+import Hallo from './GermanScreen/Hallo'
+import Ohayo from './JapaneseScreen/Ohayo'
+import BuenDia from './SpanishScreen/BuenDia'
+import GutenMorgen from './GermanScreen/GutenMorgen'
+
+//VARIABLES
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+        {/* JAPANESE */}
+        <Stack.Screen name="Konnichiwa" component={Konnichiwa} options={{headerShown: false}} />
+        <Stack.Screen name="Ohayo" component={Ohayo} options={{headerShown: false}} />
+
+        {/* SPANISH */}
+        <Stack.Screen name="Hola" component={Hola} options={{headerShown: false}} />
+        <Stack.Screen name="BuenDia" component={BuenDia} options={{headerShown: false}} />
+
+        {/* GERMAN */}
+        <Stack.Screen name="Hallo" component={Hallo} options={{headerShown: false}} />
+        <Stack.Screen name="GutenMorgen" component={GutenMorgen} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
